62a2b7be7e47ab7390d35c9be141e7e14a2aa73b
BasudevBharatBhushan <bharatbhushan173@gmail.com>
2023-01-20 10:59:22 +0530

    User & Category API Completed

---
+++ src/main/java/com/blog/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.blog.services;
+
+import java.util.List;
+
+import com.blog.payloads.CategoryDto;
+
+public interface CategoryService {
+	//Create
+	
+	CategoryDto createCategory(CategoryDto categoryDto);
+	
+	//Update
+	CategoryDto updateCategory(CategoryDto categoryDto, Integer categoryId);
+	
+	
+	//Delete
+	
+	void deleteCategory(Integer categoryId);
+	
+	//Get
+	
+	 CategoryDto getCategory(Integer categoryId);
+	
+	//Get All
+	 
+	 List<CategoryDto> getCategories();
+	
+}
