278edc0094d60756a40d75554bf9432abde2bf65
BasudevBharatBhushan <bharatbhushan173@gmail.com>
2023-02-06 12:51:22 +0530

    Comment API Added

--- src/main/java/com/blog/payloads/PostDto.java
+++ src/main/java/com/blog/payloads/PostDto.java
@@ -1,8 +1,8 @@
 package com.blog.payloads;
 
 import java.util.Date;
-
-
+import java.util.HashSet;
+import java.util.Set;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,5 +27,7 @@ public class PostDto {
 	private CategoryDto category;
 	
 	private UserDto user;
+	
+	private Set<CommentDto> comments = new HashSet<>(); 
 	
 }
